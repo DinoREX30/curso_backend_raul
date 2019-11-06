4cd0788ac660e0e90738a1e02f6c12405f9f89a9
Raul <baltazardominguez.raul@outlook.com>
2019-11-05 19:07:27 -0600

    Agregué metodo para un post de usuario

--- main.js
+++ main.js
@@ -2,9 +2,26 @@
 //Librerias
 const express = require('express')
 const colors = require('colors')
+const bodyParser = require('body-parser')
 
 //Aplicacion
 const app = express()
+
+//Configuracion de la App
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token')
+
+    res.header('Access-Control-Expose-Headers', 'x-auth-token')
+    
+    next()
+})
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 
 var port = 8000
 
@@ -12,12 +29,18 @@ app.get('/', (req, rest) => {
     rest.send('Entraste a la ruta raiz')
 })
 
-app.get('/raul',  (req, rest) => {
+app.get('/raul', (req, rest) => {
     rest.send('Hola crack')
 })
 
 app.get('/info', (req, res) => {
     res.send('Algo increible esta por venir')
+})
+
+app.post('/saludo', (req, res) => {
+    var name = req.body.name
+    
+    res.send(`Hola ${name} un gusto en conocerte`)
 })
 
 //Correr app
